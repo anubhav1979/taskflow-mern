d3eaa76625769736353cea0ff711bcf82b0dbcc6
Anubhav <anubhavsinghrathore03@gmail.com>
2026-04-17 23:38:38 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,19 @@ dotenv.config();
 const app = express();
 
 // Middleware
+// app.use(cors({
+//   origin: process.env.CLIENT_URL || 'http://localhost:3000',
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: [
+    "http://localhost:3000",
+    "https://taskflow-umber-alpha.vercel.app"
+  ],
   credentials: true
 }));
+
 app.use(express.json());
 
 // Routes
